2a45deb7747af25ceac78b64045629b3194643ce
aldo-ruben <97433363+aldo-ruben@users.noreply.github.com>
2022-06-26 11:51:04 +0200

    feat
    dev

---
+++ src/pages/help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Se você estiver com problemas para acessar um produto da explicador Inc, </p>
+    <p>É possível que estejamos enfrentando um problema temporário. </p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex'
+}
+</script>
